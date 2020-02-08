8ca39613539413fdf2461f6e0db5aa731ab28156
Kenny Mui <klmui@wisc.edu>
2020-02-08 16:59:18 -0600

    Add delete functionality

--- jQuery-Todo-App/assets/js/todos.js
+++ jQuery-Todo-App/assets/js/todos.js
@@ -2,3 +2,13 @@
 $("li").click(function(){
     $(this).toggleClass("completed");
 });
+
+// Click on X to delete Todo
+$("span").click(function(event) {
+    // this is the span
+    $(this).parent().fadeOut(function(){
+        // remove entire li
+        $(this).remove(); // Refers to li, not the span
+    }); 
+    event.stopPropagation(); // jQuery that will prevent event from bubbling up
+});
