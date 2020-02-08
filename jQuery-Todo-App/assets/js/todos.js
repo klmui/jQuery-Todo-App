c34cd09f71be25c53b3d20ecd928e8fd5d4fd532
Kenny Mui <klmui@wisc.edu>
2020-02-08 17:17:37 -0600

    Dynamically add new tasks

--- jQuery-Todo-App/assets/js/todos.js
+++ jQuery-Todo-App/assets/js/todos.js
@@ -1,14 +1,27 @@
 // Check off specific todos by clicking
-$("li").click(function(){
+
+// Target all li inside of ul
+$("ul").on("click", "li", function(){
     $(this).toggleClass("completed");
 });
 
 // Click on X to delete Todo
-$("span").click(function(event) {
+$("ul").on("click", "span", function(event) {
     // this is the span
     $(this).parent().fadeOut(function(){
         // remove entire li
         $(this).remove(); // Refers to li, not the span
     }); 
     event.stopPropagation(); // jQuery that will prevent event from bubbling up
+});
+
+// .keypress runs on any keypressed, not just enter
+$("input[type='text']").keypress(function(event){
+    // Check for enter key
+    if(event.which === 13) {
+        var todoText = $(this).val();
+        $(this).val("");
+        // Create a new li and add to ul
+        $("ul").append("<li><span>X</span> "+ todoText +"</li>");
+    }
 });
---
+++ jQuery-Todo-App/readme.md
@@ -0,0 +1,8 @@
+# jquery Todo App
+
+## Main Takeaways
+
+click() vs on('click)
+
+- click() only adds listeners for existing elements
+- on() will add listeners for all potential future elements, but make sure to attach "on" to a parent element
